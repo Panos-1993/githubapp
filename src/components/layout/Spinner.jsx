9a1dd279b1e6122c30926ef16377fd4f0e77efb3
Panos-1993 <panoslampros@icloud.com>
2022-11-22 09:26:20 +0200

    commit final

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import spinner from './assets/spinner.gif'
 
 function Spinner() {
   return (
-    <div className='w-100 mt-20'>
-        <img className='text-center mx-auto' width={180} src={spinner} alt='Loading...' />
+    <div className='mt-20'>
+      <img
+        width={180}
+        className='text-center mx-auto'
+        src={spinner}
+        alt='Loading...'
+      />
     </div>
   )
 }
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import {useContext} from 'react'
+import { useContext } from 'react'
 import Spinner from '../layout/Spinner'
-import UserItem from './UserItem'
+import UserItem from '../users/UserItem'
 import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
-    const {users, loading} = useContext(GithubContext)
+  const { users, loading } = useContext(GithubContext)
 
-    if(!loading) {
-        return (
-            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-col-3 md: grid-col-2'>
-                {users.map(user => (
-                    <UserItem key={user.id} user={user} />
-                ))}
-            </div>
-         )
-    } else {
-        return (
-            <h3>{Spinner}</h3>
-        )
-    }
-    
+  if (!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  } else {
+    return <Spinner />
+  }
 }
 
 export default UserResults
